c51277f6a449d35e4b2393ca2ba5232077090b9b
Satyam Jha <satyamjha778@gmail.com>
2025-04-15 12:26:28 +0530

    added resources link

--- src/app/mentee/dashboard/page.tsx
+++ src/app/mentee/dashboard/page.tsx
@@ -99,6 +99,19 @@ export default function MenteeDashboard() {
             </Link>
           </CardContent>
         </Card>
+        <Card className="bg-white">
+          <CardHeader>
+            <CardTitle>Access Resources</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Link
+              href={"/resources"}
+              className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
+            >
+              Link
+            </Link>
+          </CardContent>
+        </Card>
 
         <Card className="bg-white">
           <CardHeader>
